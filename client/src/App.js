f91cfd57489f999341b91790de8c0f03945ae2fe
victoriapham1 <vtp283@tamu.edu>
2022-11-04 23:18:35 -0500

    example working integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React, { Fragment } from "react";
 import "./App.css";
 
+// components
+import InputTodo from "./components/InputTodo";
+
 function App() {
-    return <Fragment></Fragment>;
+    return (
+        <Fragment>
+            <div className="container">
+                <InputTodo />
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
