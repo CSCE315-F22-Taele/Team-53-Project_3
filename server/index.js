c9fc74e86064bccbf7680a8ddcbea6761a17a952
victoriapham1 <vtp283@tamu.edu>
2022-11-05 23:06:48 -0500

    test backend routes

--- server/index.js
+++ server/index.js
@@ -6,81 +6,16 @@ const pool = require("./db");
 
 const PORT = process.env.SERVER_PORT || 5000;
 
+const indexRoute = require("./routes/index"); // Test routes
+const menuRoute = require("./routes/menu");
+
 // middleware
 app.use(cors());
 app.use(express.json()); // req.body
 
 // routes
-app.get("/api/test", (req, res) => {
-    res.json({
-        message: "Hello from Test API route!",
-    });
-});
-
-app.post("/todos", async (req, res) => {
-    try {
-        const { description } = req.body;
-        const newTodo = await pool.query(
-            "INSERT INTO teammembers (name) VALUES ($1) RETURNING *",
-            [description]
-        );
-
-        res.json(newTodo.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-app.get("/todos", async (req, res) => {
-    try {
-        const allTodo = await pool.query("SELECT * FROM teammembers");
-        res.json(allTodo.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-app.get("/todos/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const todo = await pool.query(
-            "SELECT * FROM teammembers WHERE name = $1",
-            [id]
-        );
-        res.json(todo.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-// Update a todo
-app.put("/todos/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { descrpition } = req.body;
-        const todo = await pool.query(
-            "UPDATE teammembers SET phonenumber = $1 WHERE name = $2",
-            [descrpition, id]
-        );
-        res.json("Todo was updated!");
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-// delete
-app.delete("/todos/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const todo = await pool.query(
-            "DELETE FROM teammembers WHERE name = $1",
-            [id]
-        );
-        res.json("Todo was deleted!");
-    } catch (err) {
-        console.error(err.message);
-    }
-});
+app.use("/menu", menuRoute);
+app.use("/api/index", indexRoute); // Test Routes
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`);
