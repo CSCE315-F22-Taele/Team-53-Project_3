7a4aee315f78cdb9786c2978fd697a691386ee85
victoriapham1 <vtp283@tamu.edu>
2022-11-06 00:00:30 -0500

    include db in test file and fix comments

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express.Router();
+const pool = require("../db");
 
 app.get("/", (req, res) => {
     res.json({
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express.Router();
-const pool = require("../db");
+const db = require("../db");
 
 /* Get orderid for a new order 
 To call in frontend: http://localhost:5000/api/order/getOrderId
 */
 app.get("/getOrderId", async (req, res) => {
     try {
-        // Determine orderid based on date
+        // Determine orderid based on current date
         var today = new Date();
         var tmp =
             today.getFullYear().toString() + (today.getMonth() + 1).toString();
@@ -20,7 +20,7 @@ app.get("/getOrderId", async (req, res) => {
 
         var date_int = parseInt(tmp);
 
-        const todo = await pool.query(
+        const todo = await db.query(
             "SELECT orderid FROM ordering ORDER BY orderid DESC LIMIT 1"
         );
 
@@ -44,11 +44,9 @@ To call in frontend: http://localhost:5000/api/order/getInventory
 */
 app.get("/getInventory", async (req, res) => {
     try {
-        const todo = await pool.query(
-            "SELECT * FROM inventory ORDER BY itemid"
-        );
+        const todo = await db.query("SELECT * FROM inventory ORDER BY itemid");
 
-        console.log(todo.rows);
+        // console.log(todo.rows);
         res.json(todo.rows);
     } catch (err) {
         console.error(err.message);
