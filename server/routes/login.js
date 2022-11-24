5eb32a7fca019627559278a376318e9e62d57686
victoriapham1 <vtp283@tamu.edu>
2022-11-23 22:43:56 -0600

    add employee & check if valid employee api function

--- server/routes/login.js
+++ server/routes/login.js
@@ -40,4 +40,53 @@ app.get("/isManager/:employeename", async (req, res) => {
     }
 });
 
+/***
+NOT TESTED: Need to update employee table in db.
+***/
+
+/*
+Insert a new employee.
+*/
+app.post("/insert", async (req, res) => {
+    try {
+        const { employeename, email, password } = req.body;
+
+        const hoursworked = 0;
+        const salary = 12;
+        const ismanager = false;
+
+        const todo = await db.query(
+            "INSERT INTO employee (hoursworked, salary, employeename, ismanager, email, password) VALUES ($1, $2, $3, $4, $5, $6)",
+            [hoursworked, salary, employeename, ismanager, email, password]
+        );
+
+        res.json(todo.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+/*
+Check if email & password is valid combo in employee table.
+*/
+app.get("/isEmployee/:email/:password", async (req, res) => {
+    try {
+        const email = req.params.email;
+        const password = req.params.password;
+
+        const todo = await db.query(
+            "SELECT * FROM employee WHERE email=$1 AND password=$2",
+            [email, password]
+        );
+
+        var isEmployee = false;
+        if (todo.rowCount >= 1) {
+            isEmployee = true;
+        }
+
+        res.json(isEmployee);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 module.exports = app;
