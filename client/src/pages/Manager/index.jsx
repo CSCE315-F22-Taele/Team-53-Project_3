d8e9a7994b5d290d8886ce2d9813005094536695
Annie Ren <anniexren@gmail.com>
2022-12-06 14:40:14 -0600

    sales table display

--- client/src/pages/Manager/index.jsx
+++ client/src/pages/Manager/index.jsx
@@ -3,6 +3,14 @@ import { useState, useEffect } from "react";
 import "./index.css";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 // import Stack from "@mui/material/Stack";
 //import {BrowserRouter as Router, Link, useNavigate} from 'react-router-dom';
 
@@ -22,6 +30,26 @@ export default function Manager(props) {
     
 
     const conn = "http://localhost:3500/"; // for testing
+
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+          backgroundColor: theme.palette.common.black,
+          color: theme.palette.common.white,
+        },
+        [`&.${tableCellClasses.body}`]: {
+          fontSize: 14,
+        },
+      }));
+      
+      const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+          backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+          border: 0,
+        },
+      }));
 
     const salesGet = async () => {
         
@@ -233,9 +261,31 @@ export default function Manager(props) {
 
                     </form>
 
-                    {salesShown && salesReport.map( (item) =>
-                        <li> {item.name} : {item.sold}</li>
-                    )}
+                    <br />
+
+                    <div id="salesTable" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}> 
+
+                    {salesShown && 
+                                        
+                                        <TableContainer component={Paper} sx={{ maxWidth: 400}} align="center" justifyContent="center">
+                                            <Table sx={{ maxWidth: 400}} aria-label="customized table" align="center" justifyContent="center">
+                                              <TableHead>
+                                                <TableRow>
+                                                  <StyledTableCell>Item</StyledTableCell>
+                                                  <StyledTableCell align="left">Amount Sold</StyledTableCell>
+                                                </TableRow>
+                                              </TableHead>
+                                              <TableBody>
+                                                {salesReport.map((row) => (
+                                                  <StyledTableRow key={row.name}>
+                                                    <StyledTableCell align="left">{row.name}</StyledTableCell>
+                                                    <StyledTableCell align="left">{row.sold}</StyledTableCell>
+                                                  </StyledTableRow>
+                                                ))}
+                                              </TableBody>
+                                            </Table>
+                                          </TableContainer>}
+                    </div>
 
                 </div>
 
